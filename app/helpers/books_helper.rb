eb3fd5b5baa77b9eeb8701eaf338ca4441a9c3e6
Sulav Aryal <aryalpurna6@gmail.com>
2026-01-08 14:17:08 +0545

    Added Bookshelper

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,9 @@
 module BooksHelper
+    def formatted_author(book)
+        if book.rating.to_i > 4
+            "Masterepiece by #{book.author}"
+        else
+            "by #{book.author}"
+        end
+    end
 end
