586f2d6b020ee26cf03fe0a8f1b0a55b5732502d
Sulav Aryal <aryalpurna6@gmail.com>
2026-01-09 16:05:49 +0545

    Update the checkout controller

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,7 +1,21 @@
 class CheckoutsController < ApplicationController
   def new
+    @books = Book.all
   end
 
   def create
+    result = CheckoutService.new(checkout_params).perform
+
+    if result.success?
+      flash[:notice] = "Book successfully checked out to #{result.customer.name}!"
+      redirect_to checkout_path
+    else
+      render :new
+    endrails generate controller Checkouts new create
+  end
+
+  private
+  def checkout_params
+    params.require(:checkout).permit(:book_id, :customer_email, :rented_on, :returned_on)
   end
 end
