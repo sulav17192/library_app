d1015a39c5b3cb502448ec9fbafa82197bf12d15
Sulav Aryal <aryalpurna6@gmail.com>
2026-01-09 14:55:05 +0545

    Added validation

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,17 @@
 class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :book
+
+  validates :rented_on, presence: true
+  validate :returned_on_after_rented_on
+
+  private
+
+  def returned_on_after_rented_on
+    return unless returned_on && rented_on
+
+    if returned_on < rented_on
+      errors.add(:returned_on, "must be after the rented date")
+    end
+  end
 end
